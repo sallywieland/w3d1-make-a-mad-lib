268f185f8f695013f1581395079d0401f1c14223
Sally Wieland <sallybwieland@gmail.com>
2016-10-10 19:28:27 -0400

    fourth and final commit with unnecessary comments removed

--- aboutme.js
+++ aboutme.js
@@ -1,20 +1,8 @@
-/* - First name
-- Last name
-- Email address
-- Grew up in the city (as Boolean)
-- Number of years in college (as Number)
-- High school graduation year (as Number)
-- Number of pets (as Number)
-- Number of children (as Number)
-- Favorite genre of music
-- Favorite movie */
-
 var firstName = 'Sally';
 var lastName = 'Wieland';
 var emailAddress = 'sallybwieland@gmail.com';
 var cityDweller = true;
 var yearsInCollege = 4;
-// var hsGraduation = 2011;
 var hsGraduation = new Date();
     hsGraduation.setFullYear(2011);
 var numberOfPets = 2;
